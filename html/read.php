864f47e6cd1f51147112f6e4f0d2b076d7efe61c
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-06-21 17:40:02 -0600

    Se desarrollo la pag read y se agrego un libro de ejemplo

---
+++ html/read.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lectura</title>
+    <link rel="stylesheet" href="../css/Rules.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../css/read.css" />
+</head>
+
+<body>
+    <?php
+    require_once("../html/header.php");
+    ?>
+    <main>
+        <!-- Aqui van los libros -->
+        <!-- 
+        <div id="content">
+            <embed id="book" src="../src/Metafisica_Aristoteles.pdf" type="application/pdf" />
+        </div>
+        -->
+    </main>
+    <?php
+    require_once("../html/footer.php");
+    ?>
+</body>
+
+</html>
