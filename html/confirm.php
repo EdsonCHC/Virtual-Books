b9535ba4ef9cf4f3ddeba5fc980b8f130f727842
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-06-21 14:52:25 -0600

    Se optimizo la pag Confirm

--- html/confirm.php
+++ html/confirm.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Confirm</title>
+    <title>Confirmar</title>
     <link rel="stylesheet" href="../css/Rules.css" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="../css/confirm.css" />
@@ -12,21 +12,23 @@
 
 <body>
     <main>
-        <h1 id="title">Virtual Books</h1>
-        <img id="img-ground" src="../src/login.png">
+        <h2 id="title">Virtual Books</h2>
+        <img id="img-ground" src="../src/login.png" alt="image-decoration">
         <div>
             <form>
-                <h2 id="title-form">Please confirm your account to continue</h2>
-                <p>A code will be sent to your e-mail address</p>
-                <div id="lines">
-                    <div id="line"></div>
-                    <div id="line"></div>
-                </div>
-                <div id="form-group-code">
-                    <label id="confirm-code">Confirm Code</label>
-                    <input id="number-code" type="text" placeholder="******" required>
-                    <button id="register-buttom" type="submit">Register</button>
-                </div>
+                <h2 id="title-form">Por favor, confirme su cuenta para continuar</h2>
+                    <div id="flex-lines">
+                        <div id="line"></div>
+                        <div id="line"></div>
+                    </div>
+                    <div id="form-group-code">
+                        <label id="confirm-code">Confirmar Codigo</label>
+                        <input id="number-code" type="text" placeholder="******" required>
+                        <div id="flex-buttoms">
+                            <button id="buttom" type="button">Regresar</button>
+                            <button id="buttom" type="submit">Registrar</button>
+                        </div>
+                    </div>
             </form>
         </div>
         <img id="img-ground2" src="../src/login.png">
