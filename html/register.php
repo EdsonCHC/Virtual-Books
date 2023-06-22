ea023b6e028f78e66aff175f770dd17c52d215aa
RDGuevaraA <ricardodga2005@gmail.com>
2023-06-21 18:42:46 -0600

    prueba exitosa

---
+++ html/register.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@200&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/Register.css">
+    <title>Document</title>
+</head>
+    <body>
+        <div id="mother-ctn">
+            <div id="title-ctn">
+                <h2>Virtual Books</h2>
+                <p>Register</p>
+            </div>
+
+            <img id="Ovalo_1" src="../src/login.png">
+
+            <div id="form-ctn">
+                    <form action=""> 
+                        <div id="text-ctn">
+                            <label for="">First Name</label>
+                            <input type="text">
+                            <label for="">Last Name</label>
+                            <input type="text">
+                            <label for="">E-mail</label>
+                            <input type="email">
+                            <label for="">Password</label>
+                            <input type="password">
+                            <label for="">Confirm Password</label>
+                            <input type="password">
+
+                            <div id="aparte">
+                                <p>Or continue with</p>
+                                <button><img src="../src/google.png" alt=""></button>
+                            </div>
+
+                        </div>
+                        <div id="img-ctn">
+                            <h5>Profile Picture</h5>
+                            <div><img grande src="../src/user.png" alt=""><br>
+                            </div>
+                            <button Upload>Upload image <img src="../src/img/icons8-upload-25.png" alt=""></button>
+                            <h5 primo>Select Avatar</h5>
+                            <img class="img_chiqitas" src="../src/user.png" alt="">
+                            <img class="img_chiqitas" src="../src/user.png" alt="">
+                            <img class="img_chiqitas" src="../src/user.png" alt="">
+                            <img class="img_chiqitas" src="../src/user.png" alt="">
+                        </div>
+
+                    
+                        <button id="boton" type="">Register</button>
+
+                        
+
+                       
+
+
+                    </form>
+
+                  
+            </div>
+
+
+
+
+            <img id="Ovalo_2" src="../src/login.png">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    </body>
+</html>
