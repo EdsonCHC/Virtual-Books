d29095e2307aacb10142924b56ca0ad2ae06a75f
ineedoritos <galdamezsoto13@gmail.com>
2023-08-11 08:03:45 -0600

    Esto esta malo

--- html/read.php
+++ html/read.php
@@ -18,38 +18,43 @@ if ($row->rowCount() > 0) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/Rules.css" />
-    <link rel="shortcut icon" href="../src/icons8-book-50.png" type="image/x-icon">
-    <link rel="stylesheet" href="../css/read.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/Rules.css" />
+  <link rel="shortcut icon" href="../src/icons8-book-50.png" type="image/x-icon">
+  <link rel="stylesheet" href="../css/read.css" />
 
-    <!-- Fonts and Boostrap-->
-    <script src="https://kit.fontawesome.com/7bcd40cb83.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/alertify.css">
+  <!-- Fonts and Boostrap-->
+  <script src="https://kit.fontawesome.com/7bcd40cb83.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../css/alertify.css">
 
-    <title>Lectura</title>
+  <title>Lectura</title>
 </head>
 
 <body>
+  <?php
+  require_once("../html/header.php");
+  ?>
+  <main>
     <?php
-    require_once("../html/header.php");
+    require_once("../html/aside.php");
     ?>
-    <main>
-        <?php
-        require_once("../html/aside.php");
-        ?>
-        <!-- Aqui van los libros -->
-        <div id="content">
-            <object id="book" data="<?php echo $info['src'] ?>#toolbar=0&view=fitB" type="application/pdf">
-
-            </object>
-        </div>
-    </main>
-    <?php
-    require_once("../html/footer.php");
-    ?>
+    <!-- Aqui van los libros -->
+    <div id="content">
+      <object id="book" data="<?php echo $info['src'] ?>#toolbar=0&view=fitB" type="application/pdf">
+      </object>
+      <div class="btnPart">
+        <a href="../html/book.php">
+          <button><span>Regresar</span></button>
+        </a>
+      </div>
+    </div>
+    </div>
+  </main>
+  <?php
+  require_once("../html/footer.php");
+  ?>
 </body>
 
 </html>
