be5664bf9436f51c595aedf71bc472c2a5fce2ec
RDGuevaraA <ricardodga2005@gmail.com>
2023-08-14 10:15:27 -0600

    buscador malo

--- js/buscador.js
+++ js/buscador.js
@@ -1,43 +1,6 @@
 document.addEventListener("keyup", (e) => {
-  if (e.target.matches("#buscarInpLite")) { //Literatura
-    document.querySelectorAll(".resourseLite").forEach((name) => {
-      const searchTerm = e.target.value.toLowerCase();
-      const resourceContent = name.textContent.toLowerCase();
-      const shouldShow = resourceContent.includes(searchTerm);
-
-      name.classList.toggle("filtro", shouldShow);
-      name.classList.toggle("filtro", !shouldShow);
-    });
-  } 
-  if (e.target.matches("#buscarInpCien")) { //Ciencia
-    document.querySelectorAll(".resourseCien").forEach((name) => {
-      const searchTerm = e.target.value.toLowerCase();
-      const resourceContent = name.textContent.toLowerCase();
-      const shouldShow = resourceContent.includes(searchTerm);
-
-      name.classList.toggle("filtro", shouldShow);
-      name.classList.toggle("filtro", !shouldShow);
-    });
-  } else if (e.target.matches("#buscarInpEco")) { //Economia
-    document.querySelectorAll(".resourseEco").forEach((name) => {
-      const searchTerm = e.target.value.toLowerCase();
-      const resourceContent = name.textContent.toLowerCase();
-      const shouldShow = resourceContent.includes(searchTerm);
-
-      name.classList.toggle("filtro", shouldShow);
-      name.classList.toggle("filtro", !shouldShow);
-    });
-  } else if (e.target.matches("#buscarInpFisic")) { //Fisica
-    document.querySelectorAll(".resourseFisic").forEach((name) => {
-      const searchTerm = e.target.value.toLowerCase();
-      const resourceContent = name.textContent.toLowerCase();
-      const shouldShow = resourceContent.includes(searchTerm);
-
-      name.classList.toggle("filtro", shouldShow);
-      name.classList.toggle("filtro", !shouldShow);
-    });
-  } else if (e.target.matches("#buscarInpHisto")) { //Histo
-    document.querySelectorAll(".resourseHisto").forEach((name) => {
+  if (e.target.matches("#buscarInp")) {
+    document.querySelectorAll(".resourse").forEach((name) => {
       const searchTerm = e.target.value.toLowerCase();
       const resourceContent = name.textContent.toLowerCase();
       const shouldShow = resourceContent.includes(searchTerm);
@@ -46,4 +9,42 @@ document.addEventListener("keyup", (e) => {
       name.classList.toggle("filtro", !shouldShow);
     });
   }
+  // } 
+  // if (e.target.matches("#buscarInpCien")) { //Ciencia
+  //   document.querySelectorAll(".resourseCien").forEach((name) => {
+  //     const searchTerm = e.target.value.toLowerCase();
+  //     const resourceContent = name.textContent.toLowerCase();
+  //     const shouldShow = resourceContent.includes(searchTerm);
+
+  //     name.classList.toggle("filtro", shouldShow);
+  //     name.classList.toggle("filtro", !shouldShow);
+  //   });
+  // } else if (e.target.matches("#buscarInpEco")) { //Economia
+  //   document.querySelectorAll(".resourseEco").forEach((name) => {
+  //     const searchTerm = e.target.value.toLowerCase();
+  //     const resourceContent = name.textContent.toLowerCase();
+  //     const shouldShow = resourceContent.includes(searchTerm);
+
+  //     name.classList.toggle("filtro", shouldShow);
+  //     name.classList.toggle("filtro", !shouldShow);
+  //   });
+  // } else if (e.target.matches("#buscarInpFisic")) { //Fisica
+  //   document.querySelectorAll(".resourseFisic").forEach((name) => {
+  //     const searchTerm = e.target.value.toLowerCase();
+  //     const resourceContent = name.textContent.toLowerCase();
+  //     const shouldShow = resourceContent.includes(searchTerm);
+
+  //     name.classList.toggle("filtro", shouldShow);
+  //     name.classList.toggle("filtro", !shouldShow);
+  //   });
+  // } else if (e.target.matches("#buscarInpHisto")) { //Histo
+  //   document.querySelectorAll(".resourseHisto").forEach((name) => {
+  //     const searchTerm = e.target.value.toLowerCase();
+  //     const resourceContent = name.textContent.toLowerCase();
+  //     const shouldShow = resourceContent.includes(searchTerm);
+
+  //     name.classList.toggle("filtro", shouldShow);
+  //     name.classList.toggle("filtro", !shouldShow);
+  //   });
+  // }
 });
