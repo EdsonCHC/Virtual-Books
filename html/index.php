1bb2c3a37012e935ce9d3d812ec115bf1ce960b2
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-08-15 22:49:11 -0600

    buscador 3.0

--- html/index.php
+++ html/index.php
@@ -49,8 +49,20 @@ $obj = new métodosUser();
           <h3><a href="" class="link" data-section="index" data-value="lit">Literatura</a></h3>
         </a>
         <div class="grid-books">
-          <li class="resource" id="resource">
-          </li>
+          <?php
+          $sql = "SELECT id, name, img from resource where category = 'Literatura' Limit 7";
+          $row = $obj->showData($sql);
+          if ($row->rowCount() > 0) {
+            $row->setFetchMode(PDO::FETCH_ASSOC);
+            while ($info = $row->fetch()) {
+              ?>
+              <li class="resource" id="resource">
+                
+              </li>
+              <?php
+            }
+          }
+          ?>
         </div>
       </div>
       <div class="categoría">
@@ -58,8 +70,25 @@ $obj = new métodosUser();
           <h3><a href="" class="link" data-section="index" data-value="cien">Ciencia</a></h3>
         </a>
         <div class="grid-books">
-          <li class="resource" id="resource">
-          </li>
+          <?php
+          $sql = "SELECT id, name, img from resource where category = 'Ciencia' LIMIT 7";
+          $row = $obj->showData($sql);
+          if ($row->rowCount() > 0) {
+            $row->setFetchMode(PDO::FETCH_ASSOC);
+            while ($info = $row->fetch()) {
+              ?>
+              <li class="resourse">
+                <a href="../html/book.php?id=<?php echo $info["id"] ?>">
+                  <p>
+                    <?php echo $info["name"]; ?>
+                  </p>
+                  <img src="<?php echo $info["img"]; ?>" alt="no funciona xd">
+                </a>
+              </li>
+              <?php
+            }
+          }
+          ?>
         </div>
       </div>
       <div class="categoría">
@@ -67,8 +96,25 @@ $obj = new métodosUser();
           <h3><a href="" class="link" data-section="index" data-value="eco">Economía</a></h3>
         </a>
         <div class="grid-books">
-          <li class="resource" id="resource">
-          </li>
+          <?php
+          $sql = "SELECT id, name, img from resource where category = 'Economía' LIMIT 7";
+          $row = $obj->showData($sql);
+          if ($row->rowCount() > 0) {
+            $row->setFetchMode(PDO::FETCH_ASSOC);
+            while ($info = $row->fetch()) {
+              ?>
+              <li class="resourse">
+                <a href="../html/book.php?id=<?php echo $info["id"] ?>">
+                  <p>
+                    <?php echo $info["name"]; ?>
+                  </p>
+                  <img src="<?php echo $info["img"]; ?>" alt="no funciona xd">
+                </a>
+              </li>
+              <?php
+            }
+          }
+          ?>
         </div>
       </div>
       <div class="categoría">
@@ -76,21 +122,54 @@ $obj = new métodosUser();
           <h3><a href="" class="link" data-section="index" data-value="fis">Física</a></h3>
         </a>
         <div class="grid-books">
-          <li class="resource" id="resource">
-          </li>
+          <?php
+          $sql = "SELECT id, name, img from resource where category = 'Física' Limit 7";
+          $row = $obj->showData($sql);
+          if ($row->rowCount() > 0) {
+            $row->setFetchMode(PDO::FETCH_ASSOC);
+            while ($info = $row->fetch()) {
+              ?>
+              <li class="resourse">
+                <a href="../html/book.php?id=<?php echo $info["id"] ?>">
+                  <p>
+                    <?php echo $info["name"]; ?>
+                  </p>
+                  <img src="<?php echo $info["img"]; ?>" alt="no funciona xd">
+                </a>
+              </li>
+              <?php
+            }
+          }
+          ?>
         </div>
-      </div>
-      <div class="categoria">
-        <a href="" class="tag-link">
-          <h3><a href="" class="link" data-section="index" data-value="histo">Historia</a></h3>
-        </a>
-        <div class="grid-books">
-          <li class="resource" id="resource">
-          </li>
+        <div class="categoria">
+          <a href="" class="tag-link">
+            <h3><a href="" class="link" data-section="index" data-value="histo">Historia</a></h3>
+          </a>
+          <div class="grid-books">
+            <?php
+            $sql = "SELECT id, name, img from resource where category = 'Historia' limit 7";
+            $row = $obj->showData($sql);
+            if ($row->rowCount() > 0) {
+              $row->setFetchMode(PDO::FETCH_ASSOC);
+              while ($info = $row->fetch()) {
+                ?>
+                <li class="resourse">
+                  <a href="../html/book.php?id=<?php echo $info["id"] ?>">
+                    <p>
+                      <?php echo $info["name"]; ?>
+                    </p>
+                    <img src="<?php echo $info["img"]; ?>" alt="no funciona xd">
+                  </a>
+                </li>
+                <?php
+              }
+            }
+            ?>
+          </div>
         </div>
+        <h5 id="more"><a href="" class="link" data-section="index" data-value="mas">Mirar Más...</a></h5>
       </div>
-      <h5 id="more"><a href="" class="link" data-section="index" data-value="mas">Mirar Más...</a></h5>
-    </div>
   </main>
   <?php
   require_once("../html/footer.php");
--- php/buscador.php
+++ php/buscador.php
@@ -1,44 +1,49 @@
 <?php
+/*Mimi contexto
+La cosa funciona, pero solo funciona para solo una categoria porque si la dejo sin categoria va a meter
+articulos en categorias que no debe, y para validar que ponga los articulos segun la categoria no lo se por el momento
+*/
 require_once('../php/cone.php');
 $obj = new DataBase();
 $DBH = $obj->connect();
 
-$columns = ["id", 'name', 'img'];
-$table = "resource";
-
+//Mini validación
 $searchInput = isset($_POST['buscarInp']) ? $_POST['buscarInp'] : null;
 
-$sql = "SELECT " . implode(",", $columns) . " FROM $table";
+//Consulta
+$sql = "SELECT * FROM resource";
 
 if ($searchInput !== null) {
-    $sql .= " WHERE name LIKE :searchPattern OR category LIKE :searchPattern LIMIT 1";
+    $sql .= " WHERE name LIKE :searchPattern AND category = 'Literatura' LIMIT 7"; #Solo buscaria para literatura
     $searchPattern = "%$searchInput%";
     $stmt = $DBH->prepare($sql);
     $stmt->bindParam(":searchPattern", $searchPattern, PDO::PARAM_STR);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
 
-$html = "";
-
-if (isset($result) && count($result) > 0) {
-    foreach ($result as $info) {
+    //Busca los datos relacionados
+    $html = "";
+    if (isset($result) && count($result) > 0) {
+        foreach ($result as $info) {
+            $html .= '<div class="grid-books">';
+            $html .= '<li class="resource">';
+            $html .= '<a href="../html/book.php?id=' . $info["id"] . '">';
+            $html .= '<p>' . $info['name'] . '</p>';
+            $html .= '<img src="' . $info["img"] . '" alt="No funciona xd">';
+            $html .= '</a>';
+            $html .= '</li>';
+            $html .= '</div>';
+        }
+    } else {
         $html .= '<div class="grid-books">';
         $html .= '<li class="resource">';
-        $html .= '<a href="../html/book.php?id=' . $info["id"] . '">';
-        $html .= '<p>' . $info['name'] . '</p>';
-        $html .= '<img src="' . $info["img"] . '" alt="No funciona xd">';
-        $html .= '</a>';
+        $html .= '<p>Sin resultado</p>';
         $html .= '</li>';
         $html .= '</div>';
     }
-} else {
-    $html .= '<div class="grid-books">';
-    $html .= '<li class="resource">';
-    $html .= '<p>Sin resultado</p>';
-    $html .= '</li>';
-    $html .= '</div>';
+
 }
+
 
 echo json_encode($html, JSON_UNESCAPED_UNICODE);
 ?>
