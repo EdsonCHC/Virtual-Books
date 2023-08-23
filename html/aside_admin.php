3f8bce75f7f61682933f1ecb0371dc855a1871ed
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-08-22 21:26:26 -0600

    style(Rules) Estilos aside aside_admin fix(admin_aside) Esctructura para estilos

--- html/aside_admin.php
+++ html/aside_admin.php
@@ -2,19 +2,20 @@
     <div id="home_admin">
         <h2 class="title_aside">Virtual Books Admin</h2>
         <nav id="books-nav">
-            <li><i class="fa-solid fa-house"></i>
-                <a href="../html/admin.php" class="link" data-section="AsideA" data-value="con">Contenido</a>
-                <span> ></span>
+            <li class="btnAside">
+                <i class="fa-solid fa-house"></i>
+                <a href="../html/admin.php" class="btnLink" data-section="AsideA" data-value="con">
+                    Contenido</a>
             </li>
-            <li>
+            <li class="btnAside">
                 <i class="fa-solid fa-database"></i>
-                <a href="../html/catalog_filter.php" class="link" data-section="AsideA" data-value="cat">Catálogos</a>
-                <span> ></span>
+                <a href="../html/catalog_filter.php" class="btnLink" data-section="AsideA"
+                    data-value="cat">Catálogos</a>
             </li>
         </nav>
     </div>
     <div id="settings_admin">
-    <div id="trad" onclick="toggleTrad()" >
+        <div id="trad" class="btnAside" onclick="toggleTrad()">
             <i class="fa-solid fa-globe"></i>
             <p data-section="asideU" data-value="Idiom">Idioma</p>
 
@@ -27,11 +28,21 @@
                 </div>
             </div>
         </div>
-        <a href="" class="link"><i class="fa-sharp fa-solid fa-circle-info"></i><p  data-section="AsideA" data-value="doc"> Documentación</p></a>
-        <a href="../php/log_out.php" class="link"><i class="fa-solid fa-right-from-bracket"></i> <P  data-section="AsideA" data-value="cs">Cerrar Sesión</P></a>
+        <li class="btnAside">
+            <a href="../html/aboutus.php" class="btnLink">
+                <i class="fa-sharp fa-solid fa-circle-info"></i>
+                <p data-section="AsideA" data-value="doc"> Documentación</p>
+            </a>
+        </li>
+        <li class="btnAside">
+            <a href="../php/log_out.php" class="btnLink">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <P data-section="AsideA" data-value="cs">Cerrar Sesión</P>
+            </a>
+        </li>
     </div>
 </aside>
 <script src="../js/toggle.js"></script>
-<script src="../js/trad.js"></script> 
+<script src="../js/trad.js"></script>
 <script src="https://kit.fontawesome.com/7bcd40cb83.js" crossorigin="anonymous"></script>
 <script src="../js/toggle.js"></script>
