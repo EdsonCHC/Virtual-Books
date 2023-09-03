ce299d11953b2e3e636546174c7ce1e87a07ff54
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-09-03 11:26:29 -0600

    fix(login) se me escapo el try

--- php/login_db_vb.php
+++ php/login_db_vb.php
@@ -10,39 +10,43 @@ $DBH = $obj->connect();
 
 extract($_POST);
 
-$sql = "SELECT * FROM `user` WHERE `email` = :email AND `password` = :password";
-$STH = $DBH->prepare($sql);
-$STH->bindParam(':email', $email);
-$STH->bindParam(':password', $password);
-$STH->execute();
+try {
+    $sql = "SELECT * FROM `user` WHERE `email` = :email AND `password` = :password";
+    $STH = $DBH->prepare($sql);
+    $STH->bindParam(':email', $email);
+    $STH->bindParam(':password', $password);
+    $STH->execute();
 
-if ($STH->rowCount() > 0) {
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
-    $session = $STH->fetch();
-    if ($session['rol'] === "0") {
-        session_start();
-        $_SESSION['user'] = array();
-        $_SESSION['user'][0] = $session['id'];
-        $_SESSION['user'][1] = $session['name'];
-        $_SESSION['user'][2] = $session['rol'];
+    if ($STH->rowCount() > 0) {
+        $STH->setFetchMode(PDO::FETCH_ASSOC);
+        $session = $STH->fetch();
+        if ($session['rol'] === "0") {
+            session_start();
+            $_SESSION['user'] = array();
+            $_SESSION['user'][0] = $session['id'];
+            $_SESSION['user'][1] = $session['name'];
+            $_SESSION['user'][2] = $session['rol'];
+        }
+
+        if ($session['rol'] === "1") {
+            session_start();
+            $_SESSION['admin'] = array();
+            $_SESSION['admin'][0] = $session['id'];
+            $_SESSION['admin'][1] = $session['name'];
+            $_SESSION['admin'][2] = $session['rol'];
+        }
+
+        $json = array(
+            'name' => $session['name'],
+            'rol' => $session['rol']
+        );
+
+        $json_str = json_encode($json);
+        echo $json_str;
+    } else {
+        echo "false";
     }
-
-    if ($session['rol'] === "1") {
-        session_start();
-        $_SESSION['admin'] = array();
-        $_SESSION['admin'][0] = $session['id'];
-        $_SESSION['admin'][1] = $session['name'];
-        $_SESSION['admin'][2] = $session['rol'];
-    }
-
-    $json = array(
-        'name' => $session['name'],
-        'rol' => $session['rol']
-    );
-
-    $json_str = json_encode($json);
-    echo $json_str;
-} else {
-    echo "false";
+} catch (PDOException $e) {
+    die("Error " . $e->getMessage());
 }
 ?>
